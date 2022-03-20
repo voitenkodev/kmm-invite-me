108cc43fcf1095a0b7b5f5b18e392ed36038688c
maxvoitenko <voitenko.dev@gmail.com>
2022-03-20 16:55:44 +0200

    upd gradle vers

--- build-conventions/build.gradle.kts
+++ build-conventions/build.gradle.kts
@@ -18,7 +18,7 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
-    implementation("com.android.tools.build:gradle:7.1.0-beta04")
+    implementation("com.android.tools.build:gradle:7.3.0-alpha07")
 }
 
 buildscript {
@@ -28,7 +28,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:7.1.0-beta04")
+        classpath("com.android.tools.build:gradle:7.3.0-alpha07")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
     }
 }
