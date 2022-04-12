f3878d88536b57515e6391a0b93b7bad317bf41e
maxvoitenko <voitenko.dev@gmail.com>
2022-04-12 18:44:06 +0300

    add neomorph

--- androidApp/src/main/java/com/voitenko/dev/kmminviteme/android/neomorph/neuExt.kt
+++ androidApp/src/main/java/com/voitenko/dev/kmminviteme/android/neomorph/neuExt.kt
@@ -1,15 +1,21 @@
 package com.voitenko.dev.kmminviteme.android.neomorph
 
+import android.graphics.BlurMaskFilter
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.DrawModifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.RoundRect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.ContentDrawScope
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 enum class LightSource {
-    LEFT_TOP,
-    RIGHT_TOP,
-    LEFT_BOTTOM,
-    RIGHT_BOTTOM;
+    LEFT_TOP, RIGHT_TOP, LEFT_BOTTOM, RIGHT_BOTTOM;
 
     fun opposite(): LightSource {
         return when (this) {
@@ -22,12 +28,150 @@ enum class LightSource {
 }
 
 fun Modifier.neu(
-    shape: NeuShape = Flat(RoundedCorner(0.dp)),
-    lightSource: LightSource = LightSource.LEFT_TOP
-) = this.then(
-    object : DrawModifier {
-        override fun ContentDrawScope.draw() {
-            shape.draw(this, lightSource)
-        }
+    radius: Dp = 0.dp,
+    lightSource: LightSource = LightSource.LEFT_TOP,
+    pressed: Boolean = false,
+) = this.then(object : DrawModifier {
+    override fun ContentDrawScope.draw() {
+        if (pressed) drawForeground(lightSource, radius)
+        else drawBackground(lightSource, radius)
+        drawContent()
     }
-)
+
+    fun ContentDrawScope.drawBackground(lightSource: LightSource, radius: Dp) =
+        drawIntoCanvas { canvas ->
+            val elevation = 4.dp.toPx()
+            val blurRadius = elevation * .95f
+            val displacement = elevation * .6f
+            val dark = Color.Gray.toArgb()
+            val light = Color.White.toArgb()
+
+            val paintDark = Paint().also { p ->
+                p.asFrameworkPaint().also {
+                    it.isAntiAlias = true
+                    it.isDither = true
+                    it.color = dark
+                    it.maskFilter = BlurMaskFilter(blurRadius, BlurMaskFilter.Blur.NORMAL)
+                }
+            }
+
+            val paintLight = Paint().also { p ->
+                p.asFrameworkPaint().also {
+                    it.isAntiAlias = true
+                    it.isDither = true
+                    it.color = light
+                    it.maskFilter = BlurMaskFilter(blurRadius, BlurMaskFilter.Blur.NORMAL)
+                }
+            }
+            val backgroundDarkOffset = when (lightSource.opposite()) {
+                LightSource.LEFT_TOP -> Offset(-displacement, -displacement)
+                LightSource.RIGHT_TOP -> Offset(displacement, -displacement)
+                LightSource.LEFT_BOTTOM -> Offset(-displacement, displacement)
+                LightSource.RIGHT_BOTTOM -> Offset(displacement, displacement)
+            }
+            val backgroundLightOffset = when (lightSource) {
+                LightSource.LEFT_TOP -> Offset(-displacement, -displacement)
+                LightSource.RIGHT_TOP -> Offset(displacement, -displacement)
+                LightSource.LEFT_BOTTOM -> Offset(-displacement, displacement)
+                LightSource.RIGHT_BOTTOM -> Offset(displacement, displacement)
+            }
+
+            canvas.save()
+            canvas.translate(backgroundLightOffset.x, backgroundLightOffset.y)
+            canvas.drawRoundRect(
+                0f, 0f, size.width, size.height, radius.toPx(), radius.toPx(), paintLight
+            )
+            canvas.restore()
+            canvas.save()
+            canvas.translate(backgroundDarkOffset.x, backgroundDarkOffset.y)
+            canvas.drawRoundRect(
+                0f, 0f, size.width, size.height, radius.toPx(), radius.toPx(), paintDark
+            )
+            canvas.restore()
+        }
+
+    private fun ContentDrawScope.drawForeground(lightSource: LightSource, radius: Dp) =
+        drawIntoCanvas { canvas ->
+            val elevation = 4.dp.toPx()
+            val dark = Color.Gray.toArgb()
+            val light = Color.White.toArgb()
+            val blurRadius = elevation * 0.6f
+            val strokeWidth = elevation * .95f
+            val cornerRadius = radius.toPx()
+
+            val paintDark = Paint().also { p ->
+                p.asFrameworkPaint().also { nativePaint ->
+                    nativePaint.isAntiAlias = true
+                    nativePaint.color = dark
+                    nativePaint.strokeWidth = strokeWidth
+                    nativePaint.style = android.graphics.Paint.Style.STROKE
+                    nativePaint.maskFilter = BlurMaskFilter(blurRadius, BlurMaskFilter.Blur.NORMAL)
+                }
+            }
+            val paintLight = Paint().also { p ->
+                p.asFrameworkPaint().also { nativePaint ->
+                    nativePaint.isAntiAlias = true
+                    nativePaint.color = light
+                    nativePaint.strokeWidth = strokeWidth
+                    nativePaint.style = android.graphics.Paint.Style.STROKE
+                    nativePaint.maskFilter = BlurMaskFilter(blurRadius, BlurMaskFilter.Blur.NORMAL)
+                }
+            }
+            val backgroundDarkOffset = when (lightSource) {
+                LightSource.LEFT_TOP -> Offset(strokeWidth, strokeWidth)
+                LightSource.RIGHT_TOP -> Offset(-strokeWidth, strokeWidth)
+                LightSource.LEFT_BOTTOM -> Offset(strokeWidth, -strokeWidth)
+                LightSource.RIGHT_BOTTOM -> Offset(-strokeWidth, -strokeWidth)
+            }
+            val backgroundLightOffset = when (lightSource.opposite()) {
+                LightSource.LEFT_TOP -> Offset(strokeWidth, strokeWidth)
+                LightSource.RIGHT_TOP -> Offset(-strokeWidth, strokeWidth)
+                LightSource.LEFT_BOTTOM -> Offset(strokeWidth, -strokeWidth)
+                LightSource.RIGHT_BOTTOM -> Offset(-strokeWidth, -strokeWidth)
+            }
+
+            canvas.save()
+
+            val visiblePath = Path().also { p ->
+                p.moveTo(0f, 0f)
+                p.addRoundRect(
+                    RoundRect(0f, 0f, size.width, size.height, cornerRadius, cornerRadius)
+                )
+            }
+
+            canvas.clipPath(visiblePath)
+            canvas.translate(backgroundDarkOffset.x, backgroundDarkOffset.y)
+            canvas.drawRoundRect(
+                -strokeWidth,
+                -strokeWidth,
+                this.size.width + strokeWidth,
+                this.size.height + strokeWidth,
+                cornerRadius,
+                cornerRadius,
+                paintDark
+            )
+            canvas.restore()
+
+            canvas.save()
+
+            val visiblePath2 = Path().also { p ->
+                p.moveTo(0f, 0f)
+                p.addRoundRect(
+                    RoundRect(0f, 0f, size.width, size.height, cornerRadius, cornerRadius)
+                )
+            }
+
+            canvas.clipPath(visiblePath2)
+            canvas.translate(backgroundLightOffset.x, backgroundLightOffset.y)
+            canvas.drawRoundRect(
+                -strokeWidth,
+                -strokeWidth,
+                size.width + strokeWidth,
+                size.height + strokeWidth,
+                cornerRadius,
+                cornerRadius,
+                paintLight
+            )
+            canvas.restore()
+        }
+})
--- androidApp/src/main/java/com/voitenko/dev/kmminviteme/android/ui/newevent/NewEventScreen.kt
+++ androidApp/src/main/java/com/voitenko/dev/kmminviteme/android/ui/newevent/NewEventScreen.kt
@@ -8,13 +8,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.voitenko.dev.kmminviteme.android.designsystem.components.*
-import com.voitenko.dev.kmminviteme.android.neomorph.Flat
-import com.voitenko.dev.kmminviteme.android.neomorph.Pressed
-import com.voitenko.dev.kmminviteme.android.neomorph.RoundedCorner
 import com.voitenko.dev.kmminviteme.android.neomorph.neu
 
 @Composable
@@ -33,16 +31,15 @@ fun NewEventScreen(navController: NavController) {
             modifier = Modifier
                 .fillMaxWidth()
                 .aspectRatio(1f)
-                .neu(shape = Pressed(RoundedCorner(24.dp)))
+                .background(Color.White, shape = RoundedCornerShape(24.dp))
+                .neu(24.dp, pressed = true)
                 .padding(20.dp)
         ) {
 
             Card(
                 modifier = Modifier
-                    .size(56.dp)
-                    .neu(shape = Flat(RoundedCorner(24.dp))),
-                elevation = 0.dp,
-                backgroundColor = Color.Cyan,
+                    .size(100.dp)
+                    .neu(24.dp, pressed = false),
                 shape = RoundedCornerShape(24.dp),
             ) {
                 // Add child components here.
